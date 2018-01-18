38fa88690d3132e92aeafd6d92733fb50b811d6e
brad <brad@gmail.com>
2018-01-18 22:23:03 +1300

    Added SHA512

--- Mini.hs
+++ Mini.hs
@@ -35,19 +35,19 @@ bPad8 = bPad 8 O
 bPad32 = bPad 32 O
 bPad64 = bPad 64 O
 
-bIntTo :: Int -> [B]
+bIntTo :: Integer -> [B]
 bIntTo 0 = []
 bIntTo a = bIntTo (shiftR a 1) ++ [if odd a then X else O]
-bIntTo8 = bPad8.bIntTo
-bIntTo32 = bPad32.bIntTo
-bIntTo64 = bPad64.bIntTo
+bIntTo8 x = bPad8 $ bIntTo x
+bIntTo32 x = bPad32 $ bIntTo x
+bIntTo64 x = bPad64 $ bIntTo x
 
--- bToInt :: [B] -> Int
+-- bToInt :: [B] -> Integer
 -- bToInt [] = 0
 -- bIntTo (X:xs) = bToInt xs * 2
 -- bIntTo (O:xs) = bToInt xs
 
-bToInt :: [B] -> Int
+bToInt :: [B] -> Integer
 bToInt [] = 0
 bToInt xs = 2 * bToInt (init xs) + bVal (last xs)
 
@@ -58,31 +58,31 @@ bAddNoCarry' [] _ _ = []
 bAddNoCarry' (x:xs) (y:ys) c1 = v : bAddNoCarry' xs ys c2
     where (c2, v) = bSAdd c1 x y
 
-bVal :: B -> Int
+bVal :: B -> Integer
 bVal X = 1
 bVal O = 0
 
 
-bShL :: [B] -> Int -> [B]
-bShL a 0 = a
-bShL xs a = bShL xs (a-1) ++ [O]
+bShL :: Int -> [B] -> [B]
+bShL 0 a = a
+bShL a xs = bShL (a-1) xs ++ [O]
 
 
-bShR :: [B] -> Int -> [B]
-bShR a 0 = a
-bShR xs a = O : bShR (init xs) (a-1)
+bShR :: Int -> [B] -> [B]
+bShR 0 a = a
+bShR a xs = O : bShR (a-1) (init xs)
 
 bXor = zipWith bSXor
 bAnd = zipWith bSAnd
 bOr = zipWith bSOr
 bNot = map bSNot
 
-bRotR :: [B] -> Int -> [B]
-bRotR a 0 = a
-bRotR xs a = bRotR (last xs : init xs) (a-1)
+bRotR :: Int -> [B] -> [B]
+bRotR 0 a = a
+bRotR a xs = bRotR (a-1) (last xs : init xs)
 
 
-bRotL :: [B] -> Int -> [B]
+bRotL :: [B] -> Integer -> [B]
 bRotL a 0 = a
 bRotL (x:xs) a = bRotL (xs ++ [x]) (a-1)
 
---
+++ SHAHelper.hs
@@ -0,0 +1,67 @@
+module SHAHelper where
+
+import           Data.Bits
+import           Data.Char       (intToDigit, ord)
+import           Data.List.Split
+import           Mini
+import           Numeric         (showHex)
+
+textToChunks :: Int -> String -> [[[B]]]
+textToChunks size theText = let
+    -- Pre-processing:
+    -- begin with the original message of length L bits
+    -- append a single '1' bit
+    -- append K '0' bits, where K is the minimum number >= 0 such that L + 1 + K + 64 is a multiple of 512
+    -- append L as a 64-bit big-endian integer, making the total post-processed length a multiple of 512 bits
+    msg = concatMap (bIntTo8.toInteger.ord) theText
+    addZeros = (size * 2) - mod (length msg + 1 + sizeDiv4) (size * 2)
+    -- Create the initial msg
+    msg2 = msg ++ [X] ++ replicate addZeros O ++ (bPadInt.toInteger.length) msg
+    -- Create 512byte chunks.
+    chunksLarge = chunksOf (size * 2) msg2
+        in map (chunksOf sizeDiv8) chunksLarge
+    where
+        sizeDiv4 = size `shiftR` 2
+        sizeDiv8 = size `shiftR` 3
+        bPadInt x = bPad sizeDiv4 O (bIntTo x)
+
+-- extend W step
+extendWBase :: Int -> Int -> Int -> [B] -> [B]
+extendWBase a b c x = bRotR a x `bXor` bRotR b x `bXor` bShR c x
+
+extendWStep :: ([B] -> [B]) -> ([B] -> [B]) -> [[B]] -> [B]
+extendWStep s0 s1 ws =
+    let l = length ws
+    in     (ws !! (l-16)) `bAddNoCarry`
+        s0 (ws !! (l-15)) `bAddNoCarry`
+           (ws !! (l -7)) `bAddNoCarry`
+        s1 (ws !! (l -2))
+
+extendW :: ([[B]] -> [B]) -> Int -> [[B]] -> [[B]]
+extendW extendWStep steps chunks = foldl (\wAcc _ -> wAcc ++ [extendWStep wAcc]) chunks [1..steps]
+
+
+-- Compress H func
+compressHBase :: Int -> Int -> Int -> [B] -> [B]
+compressHBase a b c x = bRotR a x `bXor` bRotR b x `bXor` bRotR c x
+
+compressHStep :: ([B] -> [B]) -> ([B] -> [B]) ->  [[B]] -> [B] -> [B] -> [[B]]
+compressHStep s0 s1 [a, b, c, d, e, f, g, h] k w =
+    let
+        ch = (e `bAnd` f) `bXor` (bNot e `bAnd` g)
+        temp1 = h `bAddNoCarry` s1 e `bAddNoCarry` ch `bAddNoCarry` k `bAddNoCarry` w
+        maj = (a `bAnd` b) `bXor` (a `bAnd` c) `bXor` (b `bAnd` c)
+        temp2 = s0 a `bAddNoCarry` maj
+    in
+        [temp1 `bAddNoCarry` temp2, a, b, c, d `bAddNoCarry` temp1, e, f, g]
+
+compressH :: ([[B]] -> [B] -> [B] -> [[B]]) -> ([[B]] -> [B]) -> [[B]] -> [[B]] -> [[B]] -> [[B]]
+compressH compressHStep extendWStep kSeed h w =
+    let hResult = foldl (\hAcc (k, w) -> compressHStep hAcc k w) h (zip kSeed (extendW extendWStep (length kSeed) w))
+    in zipWith bAddNoCarry h hResult
+
+
+textToSha :: ([[B]] -> [B] -> [B] -> [[B]]) -> ([[B]] -> [B]) -> Int -> [[B]] -> [[B]] -> String -> String
+textToSha compressHStep extendWStep size kSeed hSeed theText =
+    let results = map bToInt $ foldl (compressH compressHStep extendWStep kSeed) hSeed (textToChunks size theText)
+    in concatMap (`showHex` "") results
